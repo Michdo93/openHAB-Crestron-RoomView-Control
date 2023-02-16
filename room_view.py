90239eccb3077dbff69765ef98f28bdfb1af00cd
Michdo93 <michaeldoerflinger93@gmail.com>
2023-02-16 16:02:49 +0100

    Update room_view.py

--- room_view.py
+++ room_view.py
@@ -7,37 +7,52 @@ import sys
 pyautogui.FAILSAFE = False
 
 def openFirefox():
-    webbrowser.open("<url>", new = 0, autoraise = True)
+    webbrowser.open("http://192.168.0.59/")
 
 def closeFirefox():
     pyautogui.click(x=20, y=20, clicks=1, button="left")
     pyautogui.hotkey("alt", "f4")
 
 def togglePower():
+    pyautogui.click(x=50, y=50, clicks=1, button="left")
     pyautogui.click(x=110, y=160, clicks=1, button="left")
 
+def powerOff():
+    pyautogui.click(x=50, y=50, clicks=1, button="left")
+    pyautogui.click(x=110, y=160, clicks=1, button="left")
+    time.sleep(0.5)
+    pyautogui.click(x=345, y=345, clicks=1, button="left")
+
 def reduceVolume():
+    pyautogui.click(x=50, y=50, clicks=1, button="left")
     pyautogui.click(x=180, y=160, clicks=1, button="left")
 
 def increaseVolume():
+    pyautogui.click(x=50, y=50, clicks=1, button="left")
     pyautogui.click(x=530, y=160, clicks=1, button="left")
 
 def muteVolume():
+    pyautogui.click(x=50, y=50, clicks=1, button="left")
     pyautogui.click(x=410, y=160, clicks=1, button="left")
 
 def changeSourceToComputer1():
+    pyautogui.click(x=50, y=50, clicks=1, button="left")
     pyautogui.click(x=280, y=280, clicks=1, button="left")
 
 def changeSourceToComputer2():
+    pyautogui.click(x=50, y=50, clicks=1, button="left")
     pyautogui.click(x=280, y=320, clicks=1, button="left")
 
 def changeSourceToHDMI1():
+    pyautogui.click(x=50, y=50, clicks=1, button="left")
     pyautogui.click(x=280, y=360, clicks=1, button="left")
 
 def changeSourceToHDMI2():
+    pyautogui.click(x=50, y=50, clicks=1, button="left")
     pyautogui.click(x=280, y=400, clicks=1, button="left")
 
 def changeSourceToVideo():
+    pyautogui.click(x=50, y=50, clicks=1, button="left")
     pyautogui.click(x=280, y=440, clicks=1, button="left")
 
 if __name__ == '__main__':
